8458dc1cd4ccff7d3655b2cd216861fdbc694a8f
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-16 16:25:19 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,9 +2,10 @@ class Show < ActiveRecord::Base
 
   has_many :characters
   has_many :actors, through: :characters
+  belongs_to :network
 
   def build_network(hash)
-    self.network_id = Network.new(hash)
+    self.network = Network.new(hash)
   end
 
 end
