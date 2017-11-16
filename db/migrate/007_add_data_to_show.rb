b6be8ca64e74583e84ad299c517987136d352e42
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-16 16:03:11 +0000

    automatically backed up by learn

--- db/migrate/007_add_data_to_show.rb
+++ db/migrate/007_add_data_to_show.rb
@@ -5,5 +5,6 @@ class AddDayAndGenreToShow < ActiveRecord::Migration
   def change
     add_column :shows, :genre, :string
     add_column :shows, :day, :string
+    add_column :shows, :season, :string
   end
 end
