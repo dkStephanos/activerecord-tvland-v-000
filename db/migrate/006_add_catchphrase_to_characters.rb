28843a7cde47e25bb9f0abd4d467e43cceb6aca4
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-16 15:46:52 +0000

    automatically backed up by learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddCatchpraseToCharacters < ActiveRecord::Migration
+class AddCatchpraseToCharacter < ActiveRecord::Migration
   def change
       add_column :characters, :catchphrase, :string
   end
