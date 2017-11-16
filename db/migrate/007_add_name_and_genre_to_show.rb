eedc48dfade00917ef33879a847f62a2d4bb4568
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-16 16:01:24 +0000

    automatically backed up by learn

--- db/migrate/007_add_name_and_genre_to_show.rb
+++ db/migrate/007_add_name_and_genre_to_show.rb
@@ -1,8 +1,9 @@
-class AddNameAndGenreToShow < ActiveRecord::Migration
+class AddDayAndGenreToShow < ActiveRecord::Migration
   #to make an additive change to a schema we create
   #a new migration, and then in the change method,
   #we make the change.
   def change
     add_column :shows, :genre, :string
+    add_column :shows, :day, :string
   end
 end
